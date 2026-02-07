534aa64bfe75d9c4c262c5bc81264a749b8d84d6
sharpinsight <52362605+sharpinsight@users.noreply.github.com>
2026-02-06 22:52:51 -0600

    reestructura de json

---
+++ src/data/index.js
@@ -0,0 +1,15 @@
+// src/data/index.js
+
+// 1. Importas cada álbum individualmente
+import digimon2000NavarreteMx from "./albums/digimon-2000-navarrete-mx.json";
+import naruto2025PaniniMX from "./albums/naruto-2025-panini-mx.json";
+
+// 2. Los metes todos en un arreglo
+const albumsData = [
+  digimon2000NavarreteMx,
+  naruto2025PaniniMX,
+  
+];
+
+// 3. Exportas la lista completa para que la use la App
+export default albumsData;
